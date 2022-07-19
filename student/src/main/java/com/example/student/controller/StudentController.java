860f8ec24016de286fcc5fb7fd8b4dfc16df5bcd
Carolina Atampis <carolinanatampis@gmail.com>
2022-07-18 21:18:50 -0300

    "Examen Final"

---
+++ student/src/main/java/com/example/student/controller/StudentController.java
@@ -0,0 +1,26 @@
+package com.example.student.controller;
+
+
+import com.example.student.model.Student;
+import com.example.student.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    StudentService studentService;
+
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return studentService.getAll();
+    }
+
+}
---
+++ student/src/main/java/com/example/student/service/StudentService.java
@@ -0,0 +1,20 @@
+package com.example.student.service;
+
+
+import com.example.student.model.Student;
+import com.example.student.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    public List<Student> getAll(){
+        return studentRepository.findAll();
+    }
+}
