83ae4b1a0237c3106a10d29791c2e26a9483db15
Carolina Atampis <carolinanatampis@gmail.com>
2022-07-18 22:05:17 -0300

    "Examen Final"

--- subject/src/main/java/com/example/subject/controller/SubjectController.java
+++ subject/src/main/java/com/example/subject/controller/SubjectController.java
@@ -4,10 +4,9 @@ import com.example.subject.entity.Subject;
 import com.example.subject.service.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/subject")
@@ -15,6 +14,11 @@ public class SubjectController {
 
     @Autowired
     SubjectService subjectService;
+
+    @GetMapping
+    public List<Subject> getAll(){
+        return subjectService.getAll();
+    }
 
     @PostMapping
     public ResponseEntity<Subject> saveSubject(@RequestBody Subject subject) {
