3f3342bb79e7626e390eaeebb822fbcc2710b9e3
Carolina Atampis <carolinanatampis@gmail.com>
2022-07-18 21:13:30 -0300

    "Examen Final"

---
+++ student/src/main/java/com/example/student/model/Student.java
@@ -0,0 +1,21 @@
+package com.example.student.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullname;
+
+}
